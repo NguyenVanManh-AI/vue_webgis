1b51a7e3fb1f8da324abf16871f2d28dcda84d55
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-04-02 00:06:32 +0700

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="big">
+    <div class="item" v-for="(map, index) in maps" :key="index">
+      <iframe :src="`maps/${map}/index.html`" width="100%" height="500px"></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      maps: ['DistrictMap', 'Map', 'TrashDistrictMap', 'TrashMap']
+    }
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<style scoped>
+.big {
+  display: flex;
+  flex-wrap: wrap;
+}
+.item {
+  flex-grow: 1;
+}
+/* Your styles here */
+</style>
